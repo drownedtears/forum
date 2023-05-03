bc02df6eb57c99bd8af4bd572513f01fd70673c7
AleksandrDoketov <a.doketov@themascc.com>
2023-05-03 23:57:39 +0300

    migrations added

--- src/main/resources/migrations/schema-01.sql
+++ src/main/resources/migrations/schema-01.sql
@@ -1,23 +1,32 @@
 --liquibase formatted sql
 
 --changeset nvoxland:1
-CREATE TABLE users
-(
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(20) NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    reg_date VARCHAR(20) NOT NULL,
-    banned BOOLEAN NOT NULL
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       username VARCHAR(20) NOT NULL,
+                       password VARCHAR(20) NOT NULL,
+                       reg_date VARCHAR(20) NOT NULL,
+                       banned BOOLEAN NOT NULL
 );
+--rollback drop table users_table;
 
 --changeset nvoxland:2
 CREATE TABLE roles
 (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(20) NOT NULL
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(20) NOT NULL
 );
+--rollback drop table roles;
 
 --changeset nvoxland:3
+CREATE TABLE user_roles (
+                            user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+                            role_id INTEGER REFERENCES roles(id) ON DELETE CASCADE,
+                            PRIMARY KEY (user_id, role_id)
+);
+--rollback drop table user_roles;
+
+--changeset nvoxland:4
 CREATE TABLE articles
 (
     id SERIAL PRIMARY KEY,
@@ -25,13 +34,8 @@ CREATE TABLE articles
     content VARCHAR(255),
     rating INT NOT NULL,
     cre_date VARCHAR(20) NOT NULL,
+    author_id INT NOT NULL,
     FOREIGN KEY (author_id) REFERENCES users(id)
 
 );
-
---changeset nvoxland:4
-CREATE TABLE users_roles
-(
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (role_id) REFERENCES roles(id)
-);
+--rollback drop table articles;
